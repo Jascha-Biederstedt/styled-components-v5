de623fc505fe931251bcd3648d61907d757fb874
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-18 11:22:49 +0200

    Add global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { Button } from './common';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script'
+  }
+`;
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>Test</Button>
+    </>
+  );
 }
 
 export default App;
