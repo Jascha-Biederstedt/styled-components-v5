d54c10f294753a2936a627c9f5e6b6385b999961
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-18 13:34:00 +0200

    Add menu icon and functionality

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ReactRouterDomLink, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -19,7 +19,7 @@ const Link = ({ isActive, children, ...props }) => {
 };
 
 const Menu = styled.nav`
-  display: block;
+  display: ${p => (p.open ? 'block' : 'none')};
   font-family: 'Open Sans';
   position: absolute;
   width: 100%;
@@ -52,12 +52,36 @@ const StyledLink = styled(Link)`
   color: black;
 `;
 
+const MobileMenuIcon = styled.div`
+  margin: auto 0 auto auto;
+  width: 25px;
+  min-width: 25px;
+  padding: 5px;
+
+  > div {
+    height: 3px;
+    background: black;
+    margin: 5px 0;
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
 export const Header = () => {
   const { pathname } = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <HeaderWrapper>
-      <Menu>
+      <MobileMenuIcon onClick={() => setMenuOpen(!menuOpen)}>
+        <div />
+        <div />
+        <div />
+      </MobileMenuIcon>
+      <Menu open={menuOpen}>
         <StyledLink to="/" isActive={pathname === '/'}>
           Home
         </StyledLink>
